644e705ae7108d91d1aae20e25713cec2f4aa092
Eric Rodriguez <ericrodriguez@macbook-pro.lan>
2022-05-12 00:46:16 -0400

    updated results from text entry

--- mental_health-nlp-app.py
+++ mental_health-nlp-app.py
@@ -21,9 +21,12 @@ page = st.sidebar.selectbox(
 if page == 'About':
     st.subheader('About This Project')
     st.write('''
+
     This is a Streamlit app that showcases my classification model.
     The model utilizes natural language processing and logistic regression to predict if a reddit post came from either r/mentalhealth or r/CoronavirusUS.
+
     A model that can process text data and passively identify individuals who may require mental health care could be a valuable screening tool in order to connect these individuals with the care that they need, especially in the era of COVID.
+
     This tool demonstrates my models predictive capabilities which is a first step in creating this type of screening tool.
 
     If you enjoy this project, visit my portfolio or get in touch with me via LinkedIn!
@@ -36,6 +39,7 @@ if page == 'About':
 elif page == 'Words!':
     st.subheader('Visualizing the most common words in each subreddit')
     st.write('''
+
     Below you will find word clouds that visualize the most frequently appearing words in the data pulled from each subreddit.
 
     '''
@@ -62,9 +66,10 @@ elif page == 'Words!':
 elif page == 'Mental Health Prediction':
     st.subheader('Could this post indicate the author is in some form of mental health distress?')
     st.write('''
+
     Enter some text from a post to predict if the author of the post is in need of mental health services.
 
-    You can enter up to 500 charachters!
+    You can enter up to 500 characters!
     ''')
 
     st.write('''
@@ -88,18 +93,18 @@ elif page == 'Mental Health Prediction':
     # Labels
     my_label = 'None'
     my_asset = 'None'
-    if predicted_subreddit == 'CoronavirusUS':
+    if predicted_subreddit == 0:
         my_label = 'r/CoronavirusUS or other subreddits'
         my_asset = "not need mental health services, but you should still check on them to see if they're ok because COVID sucks too!"
-    elif predicted_subreddit == 'mentalhealth':
+    elif predicted_subreddit == 1:
         my_label = 'r/mentalhealth'
-        my_asset = 'be in need of mental health services'
+        my_asset = 'be in need of mental health services.'
 
     # Results
     st.write('''
 
     ''')
     st.subheader('Results:')
-    st.write(f'What you entered resembles text you may find on {my_label}. The author of the text you entered may {my_asset}.')
+    st.write(f'What you entered resembles text you may find on **{my_label}**. The author of the text you entered may {my_asset}')
 
 # Referenced: https://git.generalassemb.ly/DSIR-Lancelot/streamlit_lesson/blob/master/solution-code/app.py
